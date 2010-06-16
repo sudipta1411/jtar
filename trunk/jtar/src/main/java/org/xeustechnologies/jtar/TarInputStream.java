0968780a0997cde346cef205da707254729a6327
xeus.man <xeus.man@8b571c67-0528-6524-bb11-87b83592d8c3>
2010-06-16 13:37:51 +0000

    first commit

---
+++ trunk/jtar/src/main/java/org/xeustechnologies/jtar/TarInputStream.java
@@ -0,0 +1,177 @@
+/**
+ * Copyright 2010 Xeus Technologies 
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License"); 
+ * you may not use this file except in compliance with the License. 
+ * You may obtain a copy of the License at 
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0 
+ * 
+ * Unless required by applicable law or agreed to in writing, software 
+ * distributed under the License is distributed on an "AS IS" BASIS, 
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. 
+ * See the License for the specific language governing permissions and 
+ * limitations under the License. 
+ * 
+ */
+
+package org.xeustechnologies.jtar;
+
+import java.io.FilterInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+
+/**
+ * @author Kamran Zafar
+ * 
+ */
+public class TarInputStream extends FilterInputStream {
+
+    private TarEntry currentEntry;
+    private long currentFileSize;
+    private long bytesRead;
+
+    protected TarInputStream(InputStream in) {
+        super( in );
+        currentFileSize = 0;
+        bytesRead = 0;
+    }
+
+    @Override
+    public boolean markSupported() {
+        return false;
+    }
+
+    /**
+     * Not supported
+     * 
+     */
+    @Override
+    public synchronized void mark(int readlimit) {
+    }
+
+    /**
+     * Resets the stream and counters
+     * 
+     * @see java.io.FilterInputStream#reset()
+     */
+    @Override
+    public synchronized void reset() throws IOException {
+        super.reset();
+
+        bytesRead = 0;
+        currentFileSize = 0;
+        currentEntry = null;
+    }
+
+    /**
+     * Read a byte
+     * 
+     * @see java.io.FilterInputStream#read()
+     */
+    @Override
+    public int read() throws IOException {
+        byte[] buf = new byte[1];
+
+        int res = this.read( buf, 0, 1 );
+
+        if( res != -1 ) {
+            return buf[0];
+        }
+
+        return res;
+    }
+
+    /**
+     * Checks if the bytes being read exceed the entry size and adjusts the byte
+     * array length. Updates the byte counters
+     * 
+     * 
+     * @see java.io.FilterInputStream#read(byte[], int, int)
+     */
+    @Override
+    public int read(byte[] b, int off, int len) throws IOException {
+        if( currentEntry != null ) {
+            if( currentFileSize == currentEntry.getSize() ) {
+                return -1;
+            } else if( ( currentEntry.getSize() - currentFileSize ) < len ) {
+                len = (int) ( currentEntry.getSize() - currentFileSize );
+            }
+        }
+
+        int br = super.read( b, off, len );
+
+        if( br != -1 ) {
+            if( currentEntry != null ) {
+                currentFileSize += br;
+            }
+
+            bytesRead += br;
+        }
+
+        return br;
+    }
+
+    /**
+     * Returns the next entry in the org.xeustechnologies.jtar file
+     * 
+     * @return TarEntry
+     * @throws IOException
+     */
+    public TarEntry getNextEntry() throws IOException {
+        closeCurrentEntry();
+
+        byte[] header = new byte[TarConstants.HEADER_BLOCK];
+        read( header );
+
+        // Check if record is null
+        boolean eof = true;
+        for( byte b : header ) {
+            if( b != 0 ) {
+                eof = false;
+                break;
+            }
+        }
+
+        if( !eof ) {
+            currentEntry = new TarEntry( header );
+        }
+
+        return currentEntry;
+    }
+
+    /**
+     * Closes the current org.xeustechnologies.jtar entry
+     * 
+     * @throws IOException
+     */
+    protected void closeCurrentEntry() throws IOException {
+        if( currentEntry != null ) {
+            if( currentEntry.getSize() > currentFileSize ) {
+                // Not fully read skip rest of the bytes
+                skip( currentEntry.getSize() - currentFileSize );
+                bytesRead += ( currentEntry.getSize() - currentFileSize );
+            }
+
+            currentEntry = null;
+            currentFileSize = 0L;
+            skipPad();
+        }
+    }
+
+    /**
+     * Skips the pad at the end of each org.xeustechnologies.jtar entry file
+     * 
+     * @throws IOException
+     */
+    protected void skipPad() throws IOException {
+        if( bytesRead > 0 ) {
+            int extra = (int) ( bytesRead % TarConstants.DATA_BLOCK );
+
+            if( extra > 0 ) {
+                skip( TarConstants.DATA_BLOCK - extra );
+                bytesRead += ( TarConstants.DATA_BLOCK - extra );
+            }
+        }
+    }
+}
---
+++ trunk/jtar/src/test/java/org/xeustechnologies/jtar/JTarTest.java
@@ -0,0 +1,129 @@
+package org.xeustechnologies.jtar;
+
+import java.io.BufferedInputStream;
+import java.io.BufferedOutputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
+
+@RunWith(JUnit4.class)
+public class JTarTest {
+    static final int BUFFER = 2048;
+
+    /**
+     * Tar the given folder
+     * 
+     * @throws IOException
+     */
+    @Test
+    public void tar() throws IOException {
+        FileOutputStream dest = new FileOutputStream( "c:/test/test.tar" );
+        TarOutputStream out = new TarOutputStream( new BufferedOutputStream( dest ) );
+
+        tarFolder( null, "c:/test/tartest", out );
+
+        out.close();
+    }
+
+    /**
+     * Untar the tar file
+     * 
+     * @throws IOException
+     */
+    @Test
+    public void untar() throws IOException {
+        File zf = new File( "c:/test/test.tar" );
+        String destFolder = "c:/test/untartest";
+
+        BufferedOutputStream dest = null;
+        FileInputStream fis = new FileInputStream( zf );
+        TarInputStream tis = new TarInputStream( new BufferedInputStream( fis ) );
+        TarEntry entry;
+        while(( entry = tis.getNextEntry() ) != null) {
+            System.out.println( "Extracting: " + entry.getName() );
+            int count;
+            byte data[] = new byte[BUFFER];
+
+            if( entry.isDirectory() ) {
+                new File( destFolder + "/" + entry.getName() ).mkdirs();
+                continue;
+            } else {
+                new File( destFolder + "/" + entry.getName().substring( 0, entry.getName().lastIndexOf( '/' ) ) )
+                        .mkdirs();
+            }
+
+            FileOutputStream fos = new FileOutputStream( destFolder + "/" + entry.getName() );
+            dest = new BufferedOutputStream( fos, BUFFER );
+
+            while(( count = tis.read( data, 0, BUFFER ) ) != -1) {
+                dest.write( data, 0, count );
+            }
+
+            dest.flush();
+            dest.close();
+        }
+        tis.close();
+    }
+
+    public void tarFolder(String parent, String path, TarOutputStream out) {
+        try {
+            BufferedInputStream origin = null;
+            File f = new File( path );
+            String files[] = f.list();
+
+            // is file
+            if( files == null ) {
+                files = new String[1];
+                files[0] = f.getName();
+            }
+
+            parent = ( ( parent == null ) ? ( f.isFile() ) ? "" : f.getName() + "/" : parent + f.getName() + "/" );
+
+            for( int i = 0; i < files.length; i++ ) {
+                System.out.println( "Adding: " + files[i] );
+                File fe = f;
+                byte data[] = new byte[BUFFER];
+
+                if( f.isDirectory() ) {
+                    fe = new File( f, files[i] );
+                }
+
+                if( fe.isDirectory() ) {
+                    String[] fl = fe.list();
+                    if( fl != null && fl.length != 0 ) {
+                        tarFolder( parent, fe.getPath(), out );
+                    } else {
+                        TarEntry entry = new TarEntry( fe, parent + files[i] + "/" );
+                        out.putNextEntry( entry );
+                    }
+                    continue;
+                }
+
+                FileInputStream fi = new FileInputStream( fe );
+                origin = new BufferedInputStream( fi, BUFFER );
+
+                TarEntry entry = new TarEntry( fe, parent + files[i] );
+                out.putNextEntry( entry );
+
+                int count;
+                int bc = 0;
+                while(( count = origin.read( data, 0, BUFFER ) ) != -1) {
+                    out.write( data, 0, count );
+                    bc += count;
+                }
+
+                out.flush();
+
+                origin.close();
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
