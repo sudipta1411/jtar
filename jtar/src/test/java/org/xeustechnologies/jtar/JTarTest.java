736bde6834e6867e35a3c773d4da12218a65d793
xeus.man <xeus.man@8b571c67-0528-6524-bb11-87b83592d8c3>
2010-06-26 23:26:17 +0000

    updates

--- jtar/src/test/java/org/xeustechnologies/jtar/JTarTest.java
+++ jtar/src/test/java/org/xeustechnologies/jtar/JTarTest.java
@@ -1,3 +1,20 @@
+/**
+ * Copyright 2010 Xeus Technologies 
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License"); 
+ * you may not use this file except in compliance with the License. 
+ * You may obtain a copy of the License at 
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0 
+ * 
+ * Unless required by applicable law or agreed to in writing, software 
+ * distributed under the License is distributed on an "AS IS" BASIS, 
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. 
+ * See the License for the specific language governing permissions and 
+ * limitations under the License. 
+ * 
+ */
+
 package org.xeustechnologies.jtar;
 
 import java.io.BufferedInputStream;
@@ -44,7 +61,7 @@ public class JTarTest {
         FileInputStream fis = new FileInputStream( zf );
         TarInputStream tis = new TarInputStream( new BufferedInputStream( fis ) );
         TarEntry entry;
-        while(( entry = tis.getNextEntry() ) != null) {
+        while (( entry = tis.getNextEntry() ) != null) {
             System.out.println( "Extracting: " + entry.getName() );
             int count;
             byte data[] = new byte[BUFFER];
@@ -60,7 +77,7 @@ public class JTarTest {
             FileOutputStream fos = new FileOutputStream( destFolder + "/" + entry.getName() );
             dest = new BufferedOutputStream( fos );
 
-            while(( count = tis.read( data ) ) != -1) {
+            while (( count = tis.read( data ) ) != -1) {
                 dest.write( data, 0, count );
             }
 
@@ -70,60 +87,55 @@ public class JTarTest {
         tis.close();
     }
 
-    public void tarFolder(String parent, String path, TarOutputStream out) {
-        try {
-            BufferedInputStream origin = null;
-            File f = new File( path );
-            String files[] = f.list();
+    public void tarFolder(String parent, String path, TarOutputStream out) throws IOException {
+        BufferedInputStream origin = null;
+        File f = new File( path );
+        String files[] = f.list();
 
-            // is file
-            if( files == null ) {
-                files = new String[1];
-                files[0] = f.getName();
+        // is file
+        if( files == null ) {
+            files = new String[1];
+            files[0] = f.getName();
+        }
+
+        parent = ( ( parent == null ) ? ( f.isFile() ) ? "" : f.getName() + "/" : parent + f.getName() + "/" );
+
+        for( int i = 0; i < files.length; i++ ) {
+            System.out.println( "Adding: " + files[i] );
+            File fe = f;
+            byte data[] = new byte[BUFFER];
+
+            if( f.isDirectory() ) {
+                fe = new File( f, files[i] );
             }
 
-            parent = ( ( parent == null ) ? ( f.isFile() ) ? "" : f.getName() + "/" : parent + f.getName() + "/" );
-
-            for( int i = 0; i < files.length; i++ ) {
-                System.out.println( "Adding: " + files[i] );
-                File fe = f;
-                byte data[] = new byte[BUFFER];
-
-                if( f.isDirectory() ) {
-                    fe = new File( f, files[i] );
+            if( fe.isDirectory() ) {
+                String[] fl = fe.list();
+                if( fl != null && fl.length != 0 ) {
+                    tarFolder( parent, fe.getPath(), out );
+                } else {
+                    TarEntry entry = new TarEntry( fe, parent + files[i] + "/" );
+                    out.putNextEntry( entry );
                 }
-
-                if( fe.isDirectory() ) {
-                    String[] fl = fe.list();
-                    if( fl != null && fl.length != 0 ) {
-                        tarFolder( parent, fe.getPath(), out );
-                    } else {
-                        TarEntry entry = new TarEntry( fe, parent + files[i] + "/" );
-                        out.putNextEntry( entry );
-                    }
-                    continue;
-                }
-
-                FileInputStream fi = new FileInputStream( fe );
-                origin = new BufferedInputStream( fi );
-
-                TarEntry entry = new TarEntry( fe, parent + files[i] );
-                out.putNextEntry( entry );
-
-                int count;
-                int bc = 0;
-                while(( count = origin.read( data ) ) != -1) {
-                    out.write( data, 0, count );
-                    bc += count;
-                }
-
-                out.flush();
-
-                origin.close();
+                continue;
             }
 
-        } catch (Exception e) {
-            e.printStackTrace();
+            FileInputStream fi = new FileInputStream( fe );
+            origin = new BufferedInputStream( fi );
+
+            TarEntry entry = new TarEntry( fe, parent + files[i] );
+            out.putNextEntry( entry );
+
+            int count;
+            int bc = 0;
+            while (( count = origin.read( data ) ) != -1) {
+                out.write( data, 0, count );
+                bc += count;
+            }
+
+            out.flush();
+
+            origin.close();
         }
     }
 }
