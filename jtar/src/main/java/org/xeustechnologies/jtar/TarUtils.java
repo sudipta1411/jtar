b56b596d44f3bf71f28b226621d593f2e8378811
xeus.man <xeus.man@8b571c67-0528-6524-bb11-87b83592d8c3>
2010-07-01 21:56:00 +0000

    comments

--- jtar/src/main/java/org/xeustechnologies/jtar/TarUtils.java
+++ jtar/src/main/java/org/xeustechnologies/jtar/TarUtils.java
@@ -1,8 +1,35 @@
+/**
+ * Copyright 2010 Xeus Technologies 
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License"); 
+ * you may not use this file except in compliance with the License. 
+ * You may obtain a copy of the License at 
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0 
+ * 
+ * Unless required by applicable law or agreed to in writing, software 
+ * distributed under the License is distributed on an "AS IS" BASIS, 
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. 
+ * See the License for the specific language governing permissions and 
+ * limitations under the License. 
+ * 
+ */
+
 package org.xeustechnologies.jtar;
 
 import java.io.File;
 
+/**
+ * @author Kamran
+ * 
+ */
 public class TarUtils {
+    /**
+     * Determines the tar file size of the given folder/file path
+     * 
+     * @param path
+     * @return
+     */
     public static long calculateTarSize(File path) {
         return tarSize( path ) + TarConstants.EOF_BLOCK;
     }
